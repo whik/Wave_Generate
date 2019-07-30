9f665260dfe4c45987c5823b2b3519a2f8fa3a30
whik <1490789115@qq.com>
2019-07-30 19:40:53 +0800

    添加C语言文件

--- Sine_Wave_Generate/main.c
+++ Sine_Wave_Generate/main.c
@@ -1,8 +1,11 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 void GetTime(void)
 {
-    //Ê±¼ä´Á
+    //æ—¶é—´æˆ³
     time_t t;
     struct tm *pt ;
     time(&t);
@@ -12,27 +15,27 @@ void GetTime(void)
            pt->tm_year+1900, pt->tm_mon, pt->tm_mday, pt->tm_hour, pt->tm_min, pt->tm_sec);
 }
 
-//´«Èë²ÎÊı: xµãÊıx_N£¬y·¶Î§y1-y_max
+//ä¼ å…¥å‚æ•°: xç‚¹æ•°x_Nï¼ŒyèŒƒå›´y1-y_max
 void SineWaveGen(int x_N, int y_min, int y_max)
 {
     float y;
     int x;
-    int mid = (y_min + y_max) / 2;  //ÁãµãÖµ
-    int pp = mid - y_min;           //·ùÖµ
+    int mid = (y_min + y_max) / 2;  //é›¶ç‚¹å€¼
+    int pp = mid - y_min;           //å¹…å€¼
 
-    printf("/*  x·¶Î§:0-%d, y·¶Î§: %d-%d, Ô­µã: %d */\n", x_N, y_min, y_max, mid);
+    printf("/*  xèŒƒå›´:0-%d, yèŒƒå›´: %d-%d, åŸç‚¹: %d */\n", x_N, y_min, y_max, mid);
 
     FILE *fp;
 
-    fp = fopen("sine_data.txt", "w");   //´´½¨ÎÄ¼ş£¬Èç¹ûÎÄ¼şÒÑ¾­´æÔÚ£¬É¾³ıÖØĞÂ´´½¨
+    fp = fopen("sine_data.txt", "w");   //åˆ›å»ºæ–‡ä»¶ï¼Œå¦‚æœæ–‡ä»¶å·²ç»å­˜åœ¨ï¼Œåˆ é™¤é‡æ–°åˆ›å»º
 
     for(x = 0; x < x_N; x++)
     {
         y = pp * sin(2 * M_PI * x / x_N) + mid ;
-        printf("%d : data <= %d;\n", x, (int )(y+0.5)); //Êı¾İ¸ñÊ½¸ù¾İĞèÒªÖ¸¶¨
+        printf("%d : data <= %d;\n", x, (int )(y+0.5)); //æ•°æ®æ ¼å¼æ ¹æ®éœ€è¦æŒ‡å®š
         fprintf(fp, "%d : data <= %d;\n", x, (int )(y+0.5));
     }
-    //Ê±¼ä´Á
+    //æ—¶é—´æˆ³
     time_t t;
     struct tm *pt ;
     time(&t);
@@ -43,24 +46,24 @@ void SineWaveGen(int x_N, int y_min, int y_max)
     fprintf(fp, "/* Generate time: %4d-%02d-%02d %02d:%02d:%02d */\n",
             pt->tm_year+1900, pt->tm_mon, pt->tm_mday, pt->tm_hour, pt->tm_min, pt->tm_sec);
 
-    fclose(fp); //ÎÄ¼ş¹Ø±Õ
+    fclose(fp); //æ–‡ä»¶å…³é—­
 
 }
 
-//argc:²ÎÊı¸öÊı, argv:²ÎÊı±£´æµÄÊı×é
+//argc:å‚æ•°ä¸ªæ•°, argv:å‚æ•°ä¿å­˜çš„æ•°ç»„
 int main(int argc, char **argv)
 {
 //    GetTime();
     int x_N, y_min, y_max;
-    x_N = atoi(argv[1]);    //×Ö·û×ªÕûĞÍ
+    x_N = atoi(argv[1]);    //å­—ç¬¦è½¬æ•´å‹
     y_min = atoi(argv[2]);
     y_max = atoi(argv[3]);
 
-    //µãÊı×îÉÙ10¸ö£¬·ùÖµÃ»ÓĞ¸ºÊı
+    //ç‚¹æ•°æœ€å°‘10ä¸ªï¼Œå¹…å€¼æ²¡æœ‰è´Ÿæ•°
     if(argc == 4 && (x_N > 10) && (y_max > y_min) && (y_max > 0) && (y_min >0))
-        SineWaveGen(x_N, y_min, y_max);   //²¨ĞÎÉú³É
+        SineWaveGen(x_N, y_min, y_max);   //æ³¢å½¢ç”Ÿæˆ
     else
-       printf("²ÎÊı´íÎó!\n");
+       printf("å‚æ•°é”™è¯¯!\n");
 
     return 0;
 }
